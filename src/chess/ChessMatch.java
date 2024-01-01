8aff98f7c4a39d55a6ef0ca8ca10e90e183ed1e6
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2024-01-01 11:38:16 -0300

    feat - checkmate logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -15,6 +15,7 @@ public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private boolean check;
+    private boolean checkMate;
     private Board board;
 
     private List<Piece> piecesOnTheBoard = new ArrayList<>();
@@ -37,6 +38,10 @@ public class ChessMatch {
 
     public boolean getCheck() {
         return check;
+    }
+
+    public boolean getCheckMate() {
+        return checkMate;
     }
 
     public ChessPiece[][] getPieces() {
@@ -68,7 +73,11 @@ public class ChessMatch {
 
         check = (testCheck(opponent(currentPlayer))) ? true : false;
 
-        nextTurn();
+        if(testCheckMate(opponent(currentPlayer))) {
+            checkMate = true;
+        }else {
+            nextTurn();
+        }
         return (ChessPiece) capturedPiece;
     }
 
@@ -143,20 +152,38 @@ public class ChessMatch {
         return false;
     }
 
-    private void initialSetup() {
-        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new King(board, Color.WHITE));
+    private boolean testCheckMate(Color color) {
+        if(!testCheck(color)) return false;
 
-        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 8, new King(board, Color.BLACK));
+        List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+        for(Piece piece : list) {
+            boolean[][] mat = piece.possibleMoves();
+            for(int i = 0; i < board.getRows(); i++) {
+                for(int j = 0; j < board.getColumns(); j++) {
+                    if(mat[i][j]) {
+                        Position source         = ((ChessPiece)piece).getChessPosition().toPosition();
+                        Position target         = new Position(i, j);
+                        Piece    capturedPiece  = makeMove(source, target);
+                        boolean  testCheck      = testCheck(color);
+                        undoMove(source, target, capturedPiece);
+
+                        if(!testCheck) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private void initialSetup() {
+        placeNewPiece('h', 7, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 8, new King(board, Color.BLACK));
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
